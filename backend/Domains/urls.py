db370fd3166d707396a6b2738ebca8b8552c7fbe
Team4God <T4GMEDIA@MacBook-Pro-de-macbook.local>
2024-05-21 20:02:25 +0530

    added backend/Domains PUT and DELETE endpoint of domain, configured views, urls

--- backend/Domains/urls.py
+++ backend/Domains/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("", domain_view, name = "domain_list"),
     path("create/", domain_view, name = "domain_create"),
     path("<int:pk>/", domain_view, name = "domain_detail"),
+    path("update/<int:pk>/", domain_view, name = "domain_update"),
+    path("delete/<int:pk>/", domain_view, name = "domain_delete"),
 ]
--- backend/Domains/views.py
+++ backend/Domains/views.py
@@ -3,6 +3,7 @@ import json
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 
 from .models import Domain
 
@@ -56,5 +57,39 @@ def domain_view(request, pk = None, *args, **kwargs):
                 "domain_name": domain.domain_name,
             })
         return JsonResponse(data, safe = False)
+    
+    if request.method == "PUT":
+        if pk is not None:
+            try:
+                data = json.loads(request.body.decode("utf-8"))
+
+                domain = Domain.objects.get(id = pk) 
+
+                domain_name = data.get("domain_name")
+
+                if domain_name is None:
+                    return JsonResponse({"error": "domain_name field is required"})
+
+                domain.domain_name = domain_name
+
+                domain.save()
+
+                data = {
+                    "id": domain.id,
+                    "domain_name": domain.domain_name,
+                }
+                return JsonResponse(data, safe = False, status = 200)
+            except Domain.DoesNotExist:
+                return JsonResponse({"error": "Domain not found"}, status=404)
+            except json.JSONDecodeError:
+                return JsonResponse({"error": "Invalid JSON"}, status=400)
+            except Exception as e:
+                return JsonResponse({"error": str(e)}, status=500) 
+            
+    if request.method == "DELETE":
+        obj = get_object_or_404(Domain, id = pk)
+        obj.delete()
+
+        return JsonResponse({"message": "domain resource deleted successfully"}, status = 200)
 
     return JsonResponse({"error": "Method not allowed"}, status=405)
