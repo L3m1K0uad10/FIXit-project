a7dd32cb1aee33e967b4b7365506586d1f63f8ce
Team4God <T4GMEDIA@MacBook-Pro-de-macbook.local>
2024-05-22 15:53:06 +0530

    added profile GET detail POST PUT endpoint views and urls configuration files

--- backend/Professionals/urls.py
+++ backend/Professionals/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path 
-from .views import professional_view
+from .views import professional_view, profile_view
 
 urlpatterns = [
     path('', professional_view, name = "professional_list"),
@@ -7,4 +7,7 @@ urlpatterns = [
     path('create/', professional_view, name = "professional_create"),
     path('update/<int:pk>/', professional_view, name = "professional_update"),
     path('delete/<int:pk>/', professional_view, name = "professional_delete"),
+    path('profile/create/', profile_view, name = "profile_create"),
+    path('profile/<int:pk>/', profile_view, name = "profile_detail"),
+    path('profile/update/<int:pk>/', profile_view, name = "profile_update"),
 ]
