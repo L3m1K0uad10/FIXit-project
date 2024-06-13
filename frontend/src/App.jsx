d0be74979293be8bf61a58c3b94f9982103dc0de
Team4God <T4GMEDIA@MacBook-Pro-de-macbook.local>
2024-06-13 19:06:35 +0530

    Added Benefits page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import LandingPage from "./pages/landingPage/LandingPage";
 import AuthPage from "./pages/authPage/AuthPage.jsx";
 import HomePage from "./pages/homePage/HomePage";
 import ProfilesPage from "./pages/profilesPage/ProfilesPage";
+import BenefitsPage from "./pages/benefitsPage/BenefitsPage";
 
 
 function App() {
@@ -15,10 +16,7 @@ function App() {
           <Route path="/auth" element={<AuthPage />}></Route>
           <Route path="/home" element={<HomePage />}></Route>
           <Route path="/professional-profiles" element={<ProfilesPage />}></Route>
-          {/* 
-          <Route path="/USER_PAGE" element={<REG_USERPAGE />}></Route>
-          <Route path="/SERVICE" element={<SERVICE />}></Route>
-          <Route path="/CREATE" element={<CREATE />}></Route> */}
+          <Route path="/benefits" element={<BenefitsPage />}></Route>
         </Routes>
       </BrowserRouter>
     </>
