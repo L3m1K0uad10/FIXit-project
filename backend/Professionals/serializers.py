6a2eb18c5196273431ba4637eeb3243bc201ea83
Team4God <T4GMEDIA@MacBook-Pro-de-macbook.local>
2024-06-16 18:54:04 +0530

    Added Professionals App profile_view and Experience_bg_view serializers

--- backend/Professionals/serializers.py
+++ backend/Professionals/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
-from .models import Professional
+from .models import Professional, Profile, ExperienceBackground
 from Domains.models import Domain
 
 
@@ -50,3 +50,40 @@ class ProfessionalSerializer(serializers.ModelSerializer):
             'availability', 
             'rating', 
         ]
+
+class ProfileSerializer(serializers.ModelSerializer):
+    professional = serializers.PrimaryKeyRelatedField(queryset = Professional.objects.all(), write_only = True)
+    professional_details = ProfessionalSerializer(source = "professional", read_only = True)
+    
+    id = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Profile 
+        fields = [
+            'id',
+            'professional',
+            'professional_details',
+            'about',
+            'title',
+            'year_of_experience',
+        ]
+
+class ExperienceBackgroundSerializer(serializers.ModelSerializer):
+    profile = serializers.PrimaryKeyRelatedField(queryset = Profile.objects.all(), write_only = True)
+    profile_details = ProfileSerializer(source = "profile", read_only = True)
+    
+    id = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = ExperienceBackground
+        fields = [
+            'id',
+            'profile',
+            'profile_details',
+            'worked_at',
+            'duration',
+            'title',
+            'assigned_work',
+        ]
+
+
--- backend/Professionals/views.py
+++ backend/Professionals/views.py
@@ -14,14 +14,14 @@ from rest_framework import status
 
 from Domains.models import Domain
 from .models import Profile, ExperienceBackground
-from .serializers import ProfessionalSerializer
+from .serializers import ProfessionalSerializer, ProfileSerializer, ExperienceBackgroundSerializer
 
 
 # Get the custom user model
 User = get_user_model()
 
 @api_view(['GET', 'POST', 'PUT', 'DELETE']) 
-def professional_view(request, pk=None, *args, **kwargs):
+def professional_view(request, pk = None, *args, **kwargs):
 
     if request.method == "POST": # create a professional profile
         serializer = ProfessionalSerializer(data = request.data)
@@ -36,21 +36,21 @@ def professional_view(request, pk=None, *args, **kwargs):
     if request.method == "GET":
         if pk is not None:
             try:
-                professional = Professional.objects.get(pk=pk)
+                professional = Professional.objects.get(pk = pk)
                 serializer = ProfessionalSerializer(professional)
                 return Response(serializer.data)
             except Professional.DoesNotExist:
-                return Response(status=status.HTTP_404_NOT_FOUND)
+                return Response({"detail": "Professional not found"}, status=status.HTTP_404_NOT_FOUND)
         
         queryset = Professional.objects.all()
-        serializer = ProfessionalSerializer(queryset, many=True)
+        serializer = ProfessionalSerializer(queryset, many = True)
         data = serializer.data
         return Response(data)
     
     if request.method == "PUT":
         if pk is not None:
             try:
-                professional = Professional.objects.get(pk=pk)
+                professional = Professional.objects.get(pk = pk)
                 serializer = ProfessionalSerializer(professional, data=request.data, partial = True) # partial set to true give the leverage to handle partial updates, PATCH method can be used as well for partial update
                 if serializer.is_valid():
                     instance = serializer.save()
@@ -74,84 +74,36 @@ def professional_view(request, pk=None, *args, **kwargs):
 def profile_view(request, pk = None, *args, **kwargs):
 
     if request.method == "POST":
-        try:
-            data = json.loads(request.body.decode("utf-8"))
-
-            print(data)
-
-            professional_id = data.get("professional")
-            about = data.get("about")
-            title = data.get("title")
-            year_of_experience = data.get("year_of_experience")
-
-            if professional_id is None or about is None or title is None or year_of_experience is None:
-                return JsonResponse({"error": "All fields are required"}, status=400) 
-            
-            professional = Professional.objects.get(id = professional_id)
-
-            profile = Profile(
-                professional = professional,
-                about = about,
-                title = title,
-                year_of_experience = year_of_experience,
-            )
-            profile.save()
-
-            data = model_to_dict(profile)
-            return JsonResponse(data, safe = True, status = 201)
-        except json.JSONDecodeError:
-            return JsonResponse({"error": "Invalid JSON"}, status=400)
-        except Professional.DoesNotExist:
-            return JsonResponse({"error": "Professional not found"}, status=404)
-        except Exception as e:
-            return JsonResponse({"error": str(e)}, status=500)
+        serializer = ProfileSerializer(data = request.data)
+        if serializer.is_valid():
+            instance = serializer.save()
+            response_serializer = ProfileSerializer(instance)
+            data = response_serializer.data
+            return Response(data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
         
     if request.method == "GET":
         if pk is not None:
             try:
-                profile = Profile.objects.get(id = pk)
-
-                data = model_to_dict(profile)
-                return JsonResponse(data, safe = False) # if the data is not a dictionary set safe to False
+                profile = Profile.objects.get(pk = pk)
+                serializer = ProfileSerializer(profile)
+                return Response(serializer.data)
             except Profile.DoesNotExist:
-                return JsonResponse({"error": "Profile not found"}, status=404)
+                return Response({"detail": "Profile not found"}, status = status.HTTP_404_NOT_FOUND)
             
     if request.method == "PUT":
         if pk is not None:
             try:
-                data = json.loads(request.body.decode("utf-8"))
-
-                profile = Profile.objects.get(id = pk)
-
-                professional_id = data.get("professional")
-                about = data.get("about")
-                title = data.get("title")
-                year_of_experience = data.get("year_of_experience")
-
-                if professional_id is not None:
-                    professional = Professional.objects.get(id = professional_id)
-                    #print(profile.professional.id)
-                    if professional_id != profile.professional.id:
-                        return JsonResponse({"error": "Profile professional id can't be changed"})  
-                    else:
-                        profile.professional = professional                  
-                if about is not None:
-                    profile.about = about 
-                if title is not None:
-                    profile.title = title 
-                if year_of_experience is not None:
-                    profile.year_of_experience = year_of_experience
-
-                profile.save()
-
-                data = model_to_dict(profile)
-                return JsonResponse(data, safe=False, status=200)
-            except Professional.DoesNotExist:
-                return JsonResponse({"error": "Professional not found"}, status=404)
-            except json.JSONDecodeError:
-                return JsonResponse({"error": "Invalid JSON"}, status=400)
-            except Exception as e:
-                return JsonResponse({"error": str(e)}, status=500)
+                profile = Profile.objects.get(pk = pk)
+                serializer = ProfileSerializer(profile, data=request.data, partial = True) # partial set to true give the leverage to handle partial updates, PATCH method can be used as well for partial update
+                if serializer.is_valid():
+                    instance = serializer.save()
+                    response_serializer = ProfileSerializer(instance)
+                    data = response_serializer.data
+                    return Response(data, status = status.HTTP_200_OK)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            except Profile.DoesNotExist:
+                return Response({"detail": "Profile not found"}, status=status.HTTP_404_NOT_FOUND)
 
     return JsonResponse({"error": "Unsupported request method"}, status=405)
 
@@ -159,106 +111,54 @@ def profile_view(request, pk = None, *args, **kwargs):
 def experience_bg_view(request, pk = None, professional_id = None, *args, **kwargs):
 
     if request.method == "POST":
-        try:
-            data = json.loads(request.body.decode("utf-8"))
-
-            worked_at = data.get("worked_at")
-            duration = data.get("duration")
-            title = data.get("title")
-            assigned_work = data.get("assigned_work")
-            profile = data.get("profile")
-
-            if not all([worked_at, duration, title, assigned_work, profile is not None]):
-                return JsonResponse({"error": "All fields are required"}, status=400)
-
-            profile = Profile.objects.get(id = profile)
-            
-            experience_bg = ExperienceBackground(
-                worked_at = worked_at,
-                duration = duration,
-                title = title,
-                assigned_work = assigned_work,
-                profile = profile,
-            )
-            experience_bg.save()
-
-            data = model_to_dict(experience_bg)
-            return JsonResponse(data, safe=False, status=201)
-        except json.JSONDecodeError:
-            return JsonResponse({"error": "Invalid JSON"}, status=400)
-        except ExperienceBackground.DoesNotExist:
-            return JsonResponse({"error": "Experience Background not found"}, status=404)
-        except Exception as e:
-            return JsonResponse({"error": str(e)}, status=500) 
+        serializer = ExperienceBackgroundSerializer(data = request.data)
+        if serializer.is_valid():
+            instance = serializer.save()
+            response_serializer = ExperienceBackgroundSerializer(instance)
+            data = response_serializer.data
+            return Response(data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
     if request.method == "GET":
         if pk is not None:
-            print(pk)
             try:
-                experience_bg = ExperienceBackground.objects.get(id = pk) 
-                data = model_to_dict(experience_bg)
-                return JsonResponse(data, safe = False)
+                experience_bg = ExperienceBackground.objects.get(pk = pk)
+                serializer = ExperienceBackgroundSerializer(experience_bg)
+                return Response(serializer.data)
             except ExperienceBackground.DoesNotExist:
-                return JsonResponse({"error": "Review not found"}, status=404)
-            except:
-                return JsonResponse({"error": "Review not found"}, status=404)
+                return Response({"detail": "Experience Background not found"}, status=status.HTTP_404_NOT_FOUND)
 
         if professional_id is not None:
             try:
                 professional = Professional.objects.get(id = professional_id)
                 profile = Profile.objects.get(professional = professional)
-                experience_bgs = profile.experience_backgrounds.all()
+                queryset = profile.experience_backgrounds.all()
                 
-                data = []
-                for experience_bg in experience_bgs:
-                    experience_bg_data = model_to_dict(experience_bg)
-                    data.append(experience_bg_data)
-                return JsonResponse(data, safe = False)
+                serializer = ExperienceBackgroundSerializer(queryset, many = True)
+                data = serializer.data
+                return Response(data)
             except Professional.DoesNotExist:
-                return JsonResponse({"error": "Professional not found"}, status=404)
+                return Response({"detail": "Professional not found"}, status=status.HTTP_404_NOT_FOUND)
             except Profile.DoesNotExist:
-                return JsonResponse({"error": "Profile not found for this professional"}, status=404)
-            except ExperienceBackground.DoesNotExist:
-                return JsonResponse({"error": "Message not found"}, status=404)
-            except:
-                return JsonResponse({"error": "Message not found"}, status=404)
+                return Response({"detail": "Profile not found"}, status=status.HTTP_404_NOT_FOUND)
 
     if request.method == "PUT":
         if pk is not None:
             try:
-                data = json.loads(request.body.decode("utf-8"))
-
-                experience_bg = ExperienceBackground.objects.get(id = pk)
-
-                worked_at = data.get("worked_at")
-                duration = data.get("duration")
-                title = data.get("title")
-                assigned_work = data.get("assigned_work")
-                 
-                if worked_at is not None:
-                    experience_bg.worked_at = worked_at
-                if duration is not None:
-                    experience_bg.duration = duration 
-                if title is not None:
-                    experience_bg.title = title 
-                if assigned_work is not None:
-                    experience_bg.assigned_work = assigned_work
-
-                experience_bg.save()
-
-                data = model_to_dict(experience_bg)
-                return JsonResponse(data, safe=False, status=200)
-            except json.JSONDecodeError:
-                return JsonResponse({"error": "Invalid JSON"}, status=400)
+                experience_bg = ExperienceBackground.objects.get(pk = pk)
+                serializer = ExperienceBackgroundSerializer(experience_bg, data=request.data, partial = True) # partial set to true give the leverage to handle partial updates, PATCH method can be used as well for partial update
+                if serializer.is_valid():
+                    instance = serializer.save()
+                    response_serializer = ExperienceBackgroundSerializer(instance)
+                    data = response_serializer.data
+                    return Response(data, status = status.HTTP_200_OK)
+                return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
             except ExperienceBackground.DoesNotExist:
-                return JsonResponse({"error": "Experience background not found"}, status=404)
-            except Exception as e:
-                return JsonResponse({"error": str(e)}, status=500) 
+                return Response({"detail": "Experience Background not found"}, status = status.HTTP_404_NOT_FOUND)
 
     if request.method == "DELETE":
-        obj = get_object_or_404(ExperienceBackground, id = pk)
-        obj.delete()
-
-        return JsonResponse({"message": "Experience Background deleted successfully"}, status = 200)
+        instance = get_object_or_404(ExperienceBackground, pk=pk)
+        instance.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
 
     return JsonResponse({"error": "Unsupported request method"}, status=405)
