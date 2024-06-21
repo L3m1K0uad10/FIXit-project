96c0f332467aaad3f9a87ee08333bd297ff66a08
Team4God <T4GMEDIA@MacBook-Pro-de-macbook.local>
2024-06-21 20:19:31 +0530

    Updated authPage(signin and signup) for adhering a new desgin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path = "/" element = {<LandingPage />}></Route>
-          <Route path = "/auth" element = {<AuthPage />}></Route>
+          <Route path = "/auth/:page" element = {<AuthPage />}></Route>
           <Route path = "/home" element = {<HomePage />}></Route>
           <Route path = "/professional-profiles" element = {<ProfilesPage />}></Route>
           <Route path = "/benefits" element = {<BenefitsPage />}></Route>
--- frontend/src/pages/authPage/authPage.jsx
+++ frontend/src/pages/authPage/authPage.jsx
@@ -1,52 +1,127 @@
-import React from "react";
+import React, {useState} from "react";
+import { useParams } from "react-router-dom";
+
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faFacebook, faXTwitter, faInstagram} from "@fortawesome/free-brands-svg-icons"
+
 import Logo from "../../assets/Logo.png";
 import "./authPage.css";
 
+
+
 function AuthPage() {
-  return (
-    <div className="auth-container">
-      <div className="decor-1"></div>
-      <div className="decor-1-1"></div>
-      <div className="decor-2"></div>
-      <div className="decor-2-2"></div>
-      <div className="auth-core-container">
-        <img className="logo" src={Logo} alt="LOGO" />
+	let {page} = useParams();
+	const [isHovered, setIsHovered] = useState(false);
 
-        <h4>Enter Your Credentials</h4>
+	const footerStyle = {
+		width: "100%",
+		height: "85px",
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		gap: "20px",
+		backgroundColor: page === "signin" ? "#1f78b4ff" : "#023d7bff",
+		color: "white",
+	}
 
-        <div className="hr"></div>
+	const buttonBaseStyle = {
+		padding: "10px 30px",
+		cursor: "pointer",
+		marginBottom: "10px",
+		backgroundColor: page === "signin" ? "#1f78b4ff" : "#023d7bff",
+		borderColor: page === "signin" ? "#1f78b4ff" : "#023d7bff",
+		borderStyle: "solid",
+		color: "white",
+	}
 
-        <form className="sign-in-form">
-          <label>Email</label>
-          <br />
-          <input type="email" />
-          <br />
-          <label>Password</label>
-          <br />
-          <input type="password" />
-          <br />
+	const buttonHoverStyle = {
+		backgroundColor: "white",
+  		color: page === "signin" ? "#1f78b4ff" : "#023d7bff",
+	}
 
-          <p>
-            <a href="">Forgot password?</a>
-          </p>
-          <div>
-            <button type="submit">SIGN IN</button>
-          </div>
-        </form>
+	return (
+		<div className = "auth-container">
 
-        <div className="hr"></div>
+			<div className = "auth-core-container">
+				<img className = "logo" src = {Logo} alt = "LOGO" />
 
-        <p>
-          New to the platform? <a href="">Create an account</a>
-        </p>
+				{
+					page === "signin" ? (<h4>Sign In</h4>) : (<h4>Sign up</h4>)
+				}
 
-        <footer className="term-condition-container">
-          <a href="">Privacy</a>
-          <a href="">Terms</a>
-        </footer>
-      </div>
-    </div>
-  );
+				<div className = "hr hr-1" style = {page === "signin" ? {backgroundColor: "#1f78b4ff", borderColor: "#1f78b4ff"} : {backgroundColor: "#023d7bff", borderColor: "#023d7bff"}}></div>
+
+				<form className = "sign-in-form">
+					<label>Email</label>
+					<br />
+					<input type = "email" />
+					<br />
+					<label>Password</label>
+					<br />
+					<input type = "password" />
+					<br />
+					{
+						page === "signup" && (
+							<>
+								<label>Re-enter Password</label>
+								<br />
+								<input type = "password" />
+								<br />
+							</>
+						)
+					}
+					{
+						page === "signin" ? (
+							<p>
+								By creating an account you agree to our<br/><a href = "">Privacy</a> and <a href = "">Terms.</a>
+							</p>
+						) : (
+							<p>
+								<a href = "">Forgot password?</a>
+							</p>
+						)
+					}
+					<div>
+						<button 
+							type = "submit" 
+							style = {isHovered ? {...buttonBaseStyle, ...buttonHoverStyle} : buttonBaseStyle}
+							onMouseEnter = {() => setIsHovered(true)}
+							onMouseLeave = {() => setIsHovered(false)}
+						>
+							{page === "signin" ? "SIGN IN" : "SIGN UP"}
+						</button>
+					</div>
+				</form>
+
+				<div className = "hr" style = {page === "signin" ? {backgroundColor: "#1f78b4ff", borderColor: "#1f78b4ff"} : {backgroundColor: "#023d7bff", borderColor: "#023d7bff"}}></div>
+				
+				{
+					page === "signin" ? (
+						<p>
+							New to the platform? <a href = "">Create an account</a>
+						</p>
+					) : (
+						<p>
+							Already have an account? <a href = "">Sign in here</a>
+						</p>
+					)
+				}
+			</div>
+
+			<footer style = {footerStyle}>
+				<a href = "" className = "social-media-icons">
+					<FontAwesomeIcon icon = {faFacebook}/>
+				</a>
+				<a href = "" className = "social-media-icons">
+					<FontAwesomeIcon icon = {faXTwitter}/>
+				</a>
+				<a href = "" className = "social-media-icons">
+					<FontAwesomeIcon icon = {faInstagram}/>
+				</a>
+			</footer>
+		</div>
+	);
 }
 
 export default AuthPage;
+
