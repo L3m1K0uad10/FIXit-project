6be3d7d1f20e2be26df4cbc0b06471bd8e5a9e8c
Team4God <T4GMEDIA@MacBook-Pro-de-macbook.local>
2024-06-25 20:06:16 +0530

    Updated Professionals app Update/Delete views to use generics DRF views

--- backend/Professionals/views.py
+++ backend/Professionals/views.py
@@ -9,6 +9,7 @@ from .models import Profile, ExperienceBackground
 from .serializers import ProfessionalSerializer, ProfileSerializer, ExperienceBackgroundSerializer
 
 
+# Professionals views
 class ProfessionalListCreateAPIView(generics.ListCreateAPIView):
     queryset = Professional.objects.all()
     serializer_class = ProfessionalSerializer
@@ -16,56 +17,28 @@ class ProfessionalListCreateAPIView(generics.ListCreateAPIView):
 class ProfessionalDetailAPIView(generics.RetrieveAPIView):
     queryset = Professional.objects.all()
     serializer_class = ProfessionalSerializer
+    lookup_field = "pk"
 
+class ProfessionalUpdateAPIView(generics.UpdateAPIView):
+    queryset = Professional.objects.all()
+    serializer_class = ProfessionalSerializer
+    lookup_field = "pk"
 
-#@api_view(['GET', 'POST', 'PUT', 'DELETE']) 
-@api_view(['PUT', 'DELETE']) 
-def professional_view(request, pk = None, *args, **kwargs):
-
-    """ if request.method == "POST": # create a professional profile
-        serializer = ProfessionalSerializer(data = request.data)
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", True) # Ensure partial updates are allowed
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data = request.data, partial = partial)
         if serializer.is_valid():
-            instance = serializer.save()
-            response_serializer = ProfessionalSerializer(instance)
-            data = response_serializer.data
-            return Response(data, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST) """
+            self.perform_update(serializer)
+            return Response(serializer.data, status = status.HTTP_200_OK)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+class ProfessionalDestroyAPIView(generics.DestroyAPIView):
+    queryset = Professional.objects.all()
+    serializer_class = ProfessionalSerializer
+    lookup_field = "pk"
 
 
-    """ if request.method == "GET":
-        if pk is not None:
-            try:
-                professional = Professional.objects.get(pk = pk)
-                serializer = ProfessionalSerializer(professional)
-                return Response(serializer.data)
-            except Professional.DoesNotExist:
-                return Response({"detail": "Professional not found"}, status=status.HTTP_404_NOT_FOUND)
-        
-        queryset = Professional.objects.all()
-        serializer = ProfessionalSerializer(queryset, many = True)
-        data = serializer.data
-        return Response(data) """
-    
-    if request.method == "PUT":
-        if pk is not None:
-            try:
-                professional = Professional.objects.get(pk = pk)
-                serializer = ProfessionalSerializer(professional, data=request.data, partial = True) # partial set to true give the leverage to handle partial updates, PATCH method can be used as well for partial update
-                if serializer.is_valid():
-                    instance = serializer.save()
-                    response_serializer = ProfessionalSerializer(instance)
-                    data = response_serializer.data
-                    return Response(data, status = status.HTTP_200_OK)
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            except Professional.DoesNotExist:
-                return Response({"detail": "Professional not found"}, status=status.HTTP_404_NOT_FOUND)
-            
-    if request.method == "DELETE":
-        instance = get_object_or_404(Professional, pk=pk)
-        instance.delete()
-        return Response(status = status.HTTP_204_NO_CONTENT) # once delete the status code is 204 therefore No content, it is the Strict RESTful conventions(common way)
-
-    return Response({"error": "Method not allowed"}, status = status.HTTP_405_METHOD_NOT_ALLOWED)
 
 #@csrf_exempt
 @api_view(['GET', 'POST', 'PUT', 'DELETE']) 
