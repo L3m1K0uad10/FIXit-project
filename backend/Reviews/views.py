f9e5144ffc8c39cef67de1c7037c699d2d0f12e1
Team4God <T4GMEDIA@MacBook-Pro-de-macbook.local>
2024-06-26 12:35:23 +0530

    Updated Reviews app to use DRF generics views

--- backend/Reviews/views.py
+++ backend/Reviews/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -7,62 +6,38 @@ from .models import Review
 from .serializers import ReviewSerializer
 
 
-@api_view(['GET', 'POST', 'PUT', 'DELETE'])
-def review_view(request, pk = None, professional_id = None, *args, **kwargs):
-    
-    if request.method == "POST":
-        serializer = ReviewSerializer(data = request.data)
+# Review views
+class ReviewListeCreateAPIView(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+class ReviewDetailAPIView(generics.RetrieveAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    lookup_field = "pk"
+
+class ReviewListAPIView(generics.ListAPIView):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        professional_id = self.kwargs["professional_id"]
+        return Review.objects.filter(professional_id = professional_id)
+
+class ReviewUpdateAPIView(generics.UpdateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    lookup_field = "pk"
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", True)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data = request.data, partial = partial)
         if serializer.is_valid():
-            instance = serializer.save()
-            serializer_response = ReviewSerializer(instance)
-            data = serializer_response.data
-            return Response(data, status = status.HTTP_201_CREATED)
+            self.perform_update(serializer)
+            return Response(serializer.data, status = status.HTTP_200_OK)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
-    if request.method == "GET":
-        if pk is not None:
-            try:
-                review = Review.objects.get(id = pk) 
-                serializer = ReviewSerializer(review)
-                return Response(serializer.data)
-            except Review.DoesNotExist:
-                return Response({"error": "Review not found"}, status = status.HTTP_400_BAD_REQUEST)
-            
-        if professional_id is not None:
-            try:
-                queryset = Review.objects.filter(professional_id = professional_id)
-                serializer = ReviewSerializer(queryset, many = True)
-                data = serializer.data 
-                return Response(data)
-            except Review.DoesNotExist:
-                return Response({"error": "Review not found"}, status = status.HTTP_400_BAD_REQUEST)
-
-        queryset = Review.objects.all()
-        serializer = ReviewSerializer(queryset, many = True)
-        data = serializer.data
-        return Response(data)
-    
-    if request.method == "PUT":
-        # only the client user can update a specific review
-        if pk is not None:
-            try: 
-                review = Review.objects.get(id = pk)
-                serializer = ReviewSerializer(review, data = request.data, partial = True)
-                if serializer.is_valid():
-                    instance = serializer.save()
-                    serializer_response = ReviewSerializer(instance)
-                    data = serializer_response.data
-                    return Response(data, status = status.HTTP_200_OK)
-                return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-            except Review.DoesNotExist:
-                return Response({"error": "Review not found"}, status = status.HTTP_400_BAD_REQUEST)
-            
-    if request.method == "DELETE":
-        try:
-            instance = get_object_or_404(Review, id = pk)
-            instance.delete()
-            return Response(status = status.HTTP_204_NO_CONTENT)
-        except Review.DoesNotExist:
-                return Response({"error": "Review not found"}, status = status.HTTP_400_BAD_REQUEST)
-
-    return Response({"error": "Method not allowed"}, status = status.HTTP_405_METHOD_NOT_ALLOWED)
+class ReviewDestroyAPIView(generics.DestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    lookup_field = "pk"
