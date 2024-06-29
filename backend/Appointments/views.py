4b6bc17fa373330e6cf92b6e7dce31d69d3b3693
Team4God <T4GMEDIA@MacBook-Pro-de-macbook.local>
2024-06-29 21:24:51 +0530

    Added authentication and permissions of Appointments App endpoints

--- backend/Appointments/views.py
+++ backend/Appointments/views.py
@@ -1,25 +1,37 @@
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 
 from .models import Appointment
 from .serializers import AppointmentSerializer
+from Reviews.permissions import IsOwnerOrReadOnly
 
 
 # Appointment views
+""" 
+in case i'm running into any incomprehension:
+check the matching of service request user and professional 
+and appointment user and professional
+"""
 class AppointmentListCreateAPIView(generics.ListCreateAPIView):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
 
 class AppointmentDetailAPIView(generics.RetrieveAPIView):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
     lookup_field = "pk"
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
 
 class AppointmentUpdateAPIView(generics.UpdateAPIView):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
     lookup_field = "pk"
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
 
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop("partial", True)
@@ -34,4 +46,6 @@ class AppointmentDestroyAPIView(generics.DestroyAPIView):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
     lookup_field = "pk"
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
 
