bf9dc30c2711e276e1dd899e64c8b08561c9c3e3
Team4God <T4GMEDIA@MacBook-Pro-de-macbook.local>
2024-06-29 19:53:57 +0530

    Added authentication and permission of Message endpoints

--- backend/Messages/views.py
+++ backend/Messages/views.py
@@ -1,18 +1,24 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
+from rest_framework.response import Response
 
 from .models import Message
 from .serializers import MessageSerializer
+from .permissions import IsSenderOrReceiver, IsSender, IsReceiver
 
 
 # Message views
 class MessageListCreateAPIView(generics.ListCreateAPIView):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
 
 class MessageDetailAPIView(generics.RetrieveAPIView):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     lookup_field = "pk"
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated, IsSenderOrReceiver]
 
 class MessageListAPIView(generics.ListAPIView):
     serializer_class = MessageSerializer
@@ -25,8 +31,24 @@ class MessageListAPIView(generics.ListAPIView):
             receiver_id = self.kwargs["receiver_id"]
             return Message.objects.filter(receiver_id = receiver_id)
         
+    def get_permissions(self):
+        if "sender_id" in self.kwargs:
+            permission_classes = [permissions.IsAuthenticated, IsSender]
+        elif "receiver_id" in self.kwargs:
+            permission_classes = [permissions.IsAuthenticated, IsReceiver]
+        return [permission() for permission in permission_classes]
+    
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        for obj in queryset:
+            self.check_object_permissions(request, obj)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+        
 class MessageDestroyAPIView(generics.DestroyAPIView):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     lookup_field = "pk"
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated, IsSender]
 
